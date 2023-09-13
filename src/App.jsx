245d382ce113ce6bcd6806b226b13f3d760f5f74
Brasero <brandonricci4@gmail.com>
2023-09-13 10:10:15 +0200

    nettoyage avant correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css'
-import productReducer, {initialState} from "./Reducer/index.js";
-import {useReducer} from "react";
-import ProductForm from "./component/ProductForm/index.jsx";
-import List from "./component/List/index.jsx";
 
 function App() {
 
-    const [state, dispatch] = useReducer(productReducer, initialState)
 
 
     return (
         <>
-            <List items={state.products}  dispatch={dispatch}/>
-            <ProductForm state={state.product} dispatch={dispatch} />
         </>
     )
 }
--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -1,81 +1,8 @@
-let productId = 1;
 
-const initialProduct = {
-    name: '',
-    description: '',
-    prix: '',
-    qte: 1
-}
-export const initialState = {
-    products: [],
-    product: initialProduct
-}
 
 const productReducer = (state, action) => {
     switch(action.type) {
-        case 'changeValue':
-            return {
-                ...state,
-                product: {
-                    ...state.product,
-                    [action.payload.name]: action.payload.value
-                }
-            }
 
-        case 'addProduct':
-            return {
-                ...state,
-                products: [
-                    ...state.products,
-                    {
-                        id: productId++,
-                        ...action.payload
-                    }
-                ],
-                product: initialProduct
-            }
-
-        case 'suppressProduct':
-            return {
-                ...state,
-                products: state.products.filter((product) => product.id !== action.payload)
-            }
-
-        case 'increment':
-            return {
-                ...state,
-                products: state.products.map((product) => {
-                    if(product.id === action.payload) {
-                        return {
-                            ...product,
-                            qte: product.qte + 1
-                        }
-                    }
-                    return product
-                })
-            }
-
-        case 'decrement':
-            const index = state.products.findIndex((product) => product.id === action.payload)
-            if(state.products[index].qte === 1) {
-                return {
-                    ...state,
-                    products: state.products.filter((product) => product.id !== action.payload)
-                }
-            } else {
-                return {
-                    ...state,
-                    products: state.products.map((product) => {
-                        if(product.id === action.payload) {
-                            return {
-                                ...product,
-                                qte: product.qte - 1
-                            }
-                        }
-                        return product
-                    })
-                }
-            }
 
         default:
             return state;
